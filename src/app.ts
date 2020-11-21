14c6543ccd80f2d4a79d754a12d3419e7d437378
Artsiom Shylau <artsiom.shylau@sugarcrm.com>
2020-11-21 20:30:11 +0200

    attempt to fix node ts deploy

--- src/app.ts
+++ src/app.ts
@@ -3,31 +3,32 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './routes';
-import 'module-alias/register';
 import { keys } from './config';
 
 export default class App {
-  public app: express.Application;
+	public app: express.Application;
 
-  constructor() {
-    this.app = express();
-    this.setDatabases();
-    this.setAppConfig();
-  }
+	constructor() {
+		this.app = express();
+		this.setDatabases();
+		this.setAppConfig();
+	}
 
-  private setAppConfig(): void {
-    this.app.use(bodyParser.json());
-    this.app.use('/api/v1', routes);
-    this.app.get('/', (req, res) => {res.send({app: 'works'})})
-    this.app.use(bodyParser.urlencoded({ extended: true }));
-  }
+	private setAppConfig(): void {
+		this.app.use(bodyParser.json());
+		this.app.use('/api/v1', routes);
+		this.app.get('/', (req, res) => {
+			res.send({ app: 'works' });
+		});
+		this.app.use(bodyParser.urlencoded({ extended: true }));
+	}
 
-  private setDatabases(): void {
-    mongoose.connect(keys.mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
-  }
+	private setDatabases(): void {
+		mongoose.connect(keys.mongoURI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false
+		});
+	}
 }
