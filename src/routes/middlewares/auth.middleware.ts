9696ac37d1a417e6510f15308f2f69b2a6cd6305
Artsiom Shylau <artsiom.shylau@sugarcrm.com>
2020-11-21 20:35:20 +0200

    Fixed linters

--- src/routes/middlewares/auth.middleware.ts
+++ src/routes/middlewares/auth.middleware.ts
@@ -1,16 +1,18 @@
-import {Request, Response, NextFunction} from 'express'
+import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { keys } from "@src/config";
+import { keys } from '@src/config';
 
-export const auth = (req:Request, res: Response, next: NextFunction) => {
-	const token = req.header("token");
-	if (!token) return res.status(401).json({ message: "Auth Error" });
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+	const token = req.header('token');
+	if (!token) {
+		return res.status(401).json({ message: 'Auth Error' });
+	}
 	try {
-		const decoded: any= jwt.verify(token, keys.secret);
-		req.user = decoded.user;
+		const decoded: any = jwt.verify(token, keys.secret);
+		(req as any).user = decoded.user;
 		next();
 	} catch (e) {
 		console.error(e);
-		res.status(500).send({ message: "Invalid Token" });
+		res.status(500).send({ message: 'Invalid Token' });
 	}
 };
